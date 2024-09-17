bec9ced0f4c841f1ebcfac85c1daa5b3dd23913e
Aung Khant Soe <willjame12@gmail.com>
2024-09-17 21:31:03 +0630

    welcome removed

--- frontend/src/AuthPage.jsx
+++ frontend/src/AuthPage.jsx
@@ -15,7 +15,7 @@ const AuthPage = (props) => {
         <div className="background">
             <form onSubmit={onSubmit} className="form-card">
                 <div className="form-title">
-                    Welcome HiveTalk
+                    HiveTalk
                 </div>
 
                 <div className="form-subtitle">
